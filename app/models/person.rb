a1a418323a18a91dc5ef7565bc847bda5128b22e
CaseyKelly <casey.kelly@colorado.edu>
2015-02-26 19:16:53 -0700

    Completed model relationships

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
+  has_many :assignments
+  has_many :locations, through: :assignments
   validates :title, presence: :true, unless: ->(person){person.first_name.present?}
   validates :first_name, presence: :true, unless: ->(person){person.title.present?}
   validates :last_name, presence: :true
