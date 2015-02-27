d9fd34fcd2f01613e9c39eb0906f64c8b7bfb565
CaseyKelly <casey.kelly@colorado.edu>
2015-02-27 11:47:08 -0700

    Add unique location validation for assignment model

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,4 +2,12 @@ class Assignment < ActiveRecord::Base
   belongs_to :person
   belongs_to :location
   validates :role, :location_id, :person_id, presence: true
+  validate :unique_location
+
+  def unique_location
+    if person.assignments.find_by(location_id: location_id, role: role)
+     errors.add(:unique_locatoin, "People cannot be assigned to the same location with the same role")
+    end
+  end
+
 end
